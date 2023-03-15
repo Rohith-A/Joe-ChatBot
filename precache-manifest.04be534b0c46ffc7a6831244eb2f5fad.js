f22cb5ba4e46958405408ccc0efc6e7823e749ba
Rohith-A <rohithchintu1914@gmail.com>
2023-03-16 04:02:40 +0530

    Deploy React app to GitHub Pages

---
+++ precache-manifest.04be534b0c46ffc7a6831244eb2f5fad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "72f07cd755487b6749d2",
+    "url": "/Joe-ChatBot/static/css/main.8d603a79.chunk.css"
+  },
+  {
+    "revision": "72f07cd755487b6749d2",
+    "url": "/Joe-ChatBot/static/js/main.72f07cd7.chunk.js"
+  },
+  {
+    "revision": "2511f89111de37f4dae3",
+    "url": "/Joe-ChatBot/static/js/1.2511f891.chunk.js"
+  },
+  {
+    "revision": "830646e3097d1054975b",
+    "url": "/Joe-ChatBot/static/js/2.830646e3.chunk.js"
+  },
+  {
+    "revision": "b36f1a56c1e89990c42c",
+    "url": "/Joe-ChatBot/static/js/runtime~main.b36f1a56.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Joe-ChatBot/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "1b8b550d8f30438eda52eeb9b2a404db",
+    "url": "/Joe-ChatBot/static/media/bot-avatar.1b8b550d.jpg"
+  },
+  {
+    "revision": "eccae4f7a375e99b581721309cb840eb",
+    "url": "/Joe-ChatBot/index.html"
+  }
+];
