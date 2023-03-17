e88419307c05177f73330b099e95d4282ca8a064
Rohith-A <rohithchintu1914@gmail.com>
2023-03-17 18:02:31 +0530

    Updates

---
+++ precache-manifest.ff66ea63f7af21ed0e9220efec0a1353.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "035aa4360593887ffb6c",
+    "url": "/Joe-ChatBot/static/css/main.12139172.chunk.css"
+  },
+  {
+    "revision": "035aa4360593887ffb6c",
+    "url": "/Joe-ChatBot/static/js/main.df03344b.chunk.js"
+  },
+  {
+    "revision": "0bb180a6c8d329e86ec8",
+    "url": "/Joe-ChatBot/static/js/runtime~main.8dccdc76.js"
+  },
+  {
+    "revision": "31ca4ca63a13fb349d11",
+    "url": "/Joe-ChatBot/static/js/2.2fd2317a.chunk.js"
+  },
+  {
+    "revision": "7172cba3a9d09b71327a",
+    "url": "/Joe-ChatBot/static/js/3.e7d04950.chunk.js"
+  },
+  {
+    "revision": "ebb3e41ba886a4bd5038",
+    "url": "/Joe-ChatBot/static/js/4.207ec1a7.chunk.js"
+  },
+  {
+    "revision": "64005d0d44893e830156",
+    "url": "/Joe-ChatBot/static/js/5.c9c8d2fa.chunk.js"
+  },
+  {
+    "revision": "a8039d3c92abbe9abef3",
+    "url": "/Joe-ChatBot/static/js/6.6aa0519e.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Joe-ChatBot/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "1b8b550d8f30438eda52eeb9b2a404db",
+    "url": "/Joe-ChatBot/static/media/bot-avatar.1b8b550d.jpg"
+  },
+  {
+    "revision": "1dc89c025fb742d0cc6b618c5bbda7da",
+    "url": "/Joe-ChatBot/index.html"
+  }
+];
